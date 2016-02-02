619521bf60e4e613b4104b17bdb8d0583904a52a
bhanu <bhanu@impingeonline.com>
2016-02-02 15:39:29 +0530

    mail

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 App1::Application.initialize!
+  config.action_mailer.default_url_options = { host: 'bhanublog.herokuapp.com', port: 3000 }
+
 
 require 'carrierwave/orm/activerecord'
